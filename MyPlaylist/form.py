b646712c56b430cbb0f207ec015047ce47a40a52
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-04-27 01:52:07 +0100

    Delete form.py

--- MyPlaylist/form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed
-from wtforms import FileField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class UploadMusic(FlaskForm):
-    music = FileField('Upload Music', validators=[FileAllowed(['mp3']), DataRequired()])
-    submit = SubmitField('Upload')
