7c52662991214f5f30fe86207ff80b9dd89f1d6d
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-04-27 02:04:51 +0100

    Update run.py

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def save_img(form_photo):
         random_hex = secrets.token_hex(8)
         _, f_ext = os.path.splitext(form_photo.filename)
         path = random_hex + f_ext
-        picture_path = os.path.join(current_app.root_path, 'static/MyMusic', path)
+        picture_path = os.path.join(current_app.root_path, 'static/MyPlaylist', path)
         size = (625, 625)
         form_photo.save(picture_path)
 
