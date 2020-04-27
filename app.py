1e55d9214099a53d2e3589cd7d222c9d552c80ec
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-04-27 03:05:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import secrets
 from flask import current_app
 
 app.secret_key = 'asdnafnj#46sjsnvd(*$43sfjkndkjvnskb6441531@#$$6sddf'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://isyrbsxhlmykke:264c21cf7c770bfbe586fdd4c413bebb6da3fa34f154a262d8dd73de2ea1d2a7@ec2-34-225-82-212.compute-1.amazonaws.com:5432/d7ct20f1f2urjn'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
@@ -21,7 +21,7 @@ def save_img(form_photo):
         random_hex = secrets.token_hex(8)
         _, f_ext = os.path.splitext(form_photo.filename)
         path = random_hex + f_ext
-        picture_path = os.path.join(current_app.root_path, 'static/MyMusic', path)
+        picture_path = os.path.join(current_app.root_path, 'static/MyPlaylist', path)
         size = (625, 625)
         form_photo.save(picture_path)
 
